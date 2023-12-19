952e320909fea7f92de4245d9ac7c841156b267e
Mandy Liu <manling512@gmail.com>
2023-12-18 19:38:47 -0700

    Use Axios to interact with backend API

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,15 +1,36 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
+  const [updatedUser, setUpdatedUser] = useState(null);
+
+  useEffect(() => {
+    const updateUserProfile = () => {
+      if (isAuthenticated) {
+        axios.post('/api/user', { user })
+          .then(response => {
+            console.log("User profile updated:", response.data);
+            setUpdatedUser(response.data);
+          })
+          .catch(error => {
+            console.error("Error updating user profile:", error);
+          });
+      }
+    };
+
+    updateUserProfile();
+  }, [isAuthenticated, user]);
 
   return (
-    isAuthenticated && (
+    updatedUser && (
       <article>
-        {user?.picture && <img src={user.picture} alt={user?.name} />}
-        <h2>{user?.name}</h2>
+        {updatedUser?.picture && <img src={updatedUser.picture} alt={updatedUser?.name} />}
+        <h2>{updatedUser?.name}</h2>
         <ul>
-          {Object.keys(user).map((objKey, i) => <li key={i}>{objKey}: {user[objKey]}</li>)}
+          {Object.keys(updatedUser).map((objKey, i) => <li key={i}>{objKey}: {updatedUser[objKey]}</li>)}
         </ul>
       </article>
     )
