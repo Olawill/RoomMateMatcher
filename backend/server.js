7d9f0ecc619fbafd9119224a5d898acb14256ff8
Olawale <henry.williams658@gmail.com>
2023-12-19 03:23:30 +0000

    Add routes to server

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,56 @@
 
 require('dotenv').config();
-const Express = require('express');
-const App = Express();
-const BodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
 const helmet = require("helmet");
 const cors = require("cors");
-const userRoutes = require('./routes/userRoutes');
 const PORT = process.env.PORT || 8003;
 
 const db = require('./db/connection');
+
+// ROUTES
+const userRoutes = require('./routes/userRoutes');
 const listingRoutes = require('./routes/listingRoutes');
 
-// Express Configuration
-App.use(cors());
-App.use(helmet())
-App.use(BodyParser.urlencoded({ extended: false }));
-App.use(BodyParser.json());
-App.use(Express.static('public'));
+// express Configuration
+app.use(cors());
+app.use(helmet())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// SOCKET.IO
+// Socket.io connection
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+
+  // Handle the notification event when the client emits it
+  socket.on('notification', (data) => {
+    console.log('Notification data received:', data);
+    // Handle the notification, for example, send it to the specified user
+    const {listingUserId, userId, message} = data;
+    io.to(listingUserId).emit('notification', { from: userId, message: message });
+  });
+
+  // Handle disconnecting from socekt.io
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+});
 
 // Sample GET route
-App.get('/api/data', (req, res) => res.json({
+app.get('/api/data', (req, res) => res.json({
   message: "Seems to work!",
 }));
 
-App.use('/api', userRoutes(db));
-App.use('/api', listingRoutes(db));
+app.use('/api', userRoutes(db));
+app.use('/api', listingRoutes(db));
 
-App.listen(PORT, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`App listening on port ${PORT} so that's pretty good 👍`);
+  console.log(`app listening on port ${PORT} so that's pretty good 👍`);
 });
