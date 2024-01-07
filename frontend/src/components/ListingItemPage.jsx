cb9d49a59844059db32ca4bf0b4d470f1772f7fc
Mandy Liu <manling512@gmail.com>
2024-01-06 18:11:07 -0700

    Import navbar into listing item page

--- frontend/src/components/ListingItemPage.jsx
+++ frontend/src/components/ListingItemPage.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Container, Row, Col, Card, Alert, Button } from "react-bootstrap";
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import NavigationBar from "./NavigationBar";
 import ReviewForm from "./ReviewForm";
 import Reviews from "./Reviews";
 
@@ -24,7 +25,7 @@ const ListingItemPage = () => {
         setListingDetails(response.data.listing);
         setReviews(response.data.reviews);
       } catch (error) {
-        console.error('Error fetching listing details and reviews:', error);
+        console.error("Error fetching listing details and reviews:", error);
       }
     };
 
@@ -33,68 +34,76 @@ const ListingItemPage = () => {
 
   const handleReviewFormSubmit = async (reviewData) => {
     if (!isAuthenticated) {
-      alert('Please login first');
+      alert("Please login first");
       loginWithRedirect();
       return;
     }
-    
+
     try {
       // Post the review to backend database
       await axios.post(`/api/listings/${listing_id}/reviews`, reviewData);
       // Refetch all reviews after posting a new review
       const response = await axios.get(`/api/listings/${listing_id}`);
       setReviews(response.data.reviews);
-      setReviewNotification('Review posted successfully!');
+      setReviewNotification("Review posted successfully!");
 
       // Display the notification for 3 seconds and then clear it
       setTimeout(() => {
         setReviewNotification(null);
       }, 3000);
     } catch (error) {
-      console.error('Error posting review:', error);
+      console.error("Error posting review:", error);
     }
   };
 
   return (
-    <Container>
-      {listingDetails && (
+    <>
+      <NavigationBar />
+      <Container>
+        {listingDetails && (
+          <Row>
+            <Col>
+              <Card>
+                <Card.Img variant="top" src={listingDetails.image_url} style={{  width: '65rem', margin: '5.5rem auto 0' }} />
+                <Card.Body>
+                  <Card.Title>{listingDetails.title}</Card.Title>
+                  <Card.Text>
+                    {listingDetails.city} {listingDetails.country}
+                  </Card.Text>
+                  <Card.Text>${listingDetails.price} CAD per month</Card.Text>
+                  <Card.Text>{listingDetails.description}</Card.Text>
+                  <Card.Text>
+                    We have {listingDetails.number_of_rooms} bedrooms.
+                  </Card.Text>
+                  <Card.Text>
+                    We are looking for {listingDetails.number_of_roommates}{" "}
+                    roommates.
+                  </Card.Text>
+                  <Card.Text>Status: {listingDetails.status}</Card.Text>
+                </Card.Body>
+                <Button type="submit">Interested</Button>
+              </Card>
+            </Col>
+          </Row>
+        )}
+
+        {reviews.length > 0 && <Reviews reviews={reviews} />}
+
+        {reviewNotification && (
+          <Row>
+            <Col>
+              <Alert variant="success">{reviewNotification}</Alert>
+            </Col>
+          </Row>
+        )}
+
         <Row>
           <Col>
-            <Card>
-              <Card.Img variant="top" src={listingDetails.image_url} />
-              <Card.Body>
-                <Card.Title>{listingDetails.title}</Card.Title>
-                <Card.Text>
-                {listingDetails.city} {listingDetails.country}
-                </Card.Text>
-                <Card.Text>${listingDetails.price} CAD per month</Card.Text>
-                <Card.Text>{listingDetails.description}</Card.Text>
-                <Card.Text>We have {listingDetails.number_of_rooms} bedrooms.</Card.Text>
-                <Card.Text>We are looking for {listingDetails.number_of_roommates} roommates.</Card.Text>
-                <Card.Text>Status: {listingDetails.status}</Card.Text>
-              </Card.Body>
-              <Button type='submit'>Interested</Button>
-            </Card>
+            <ReviewForm handleReviewFormSubmit={handleReviewFormSubmit} />
           </Col>
         </Row>
-      )}
-
-      {reviews.length > 0 && <Reviews reviews={reviews} />}
-
-      {reviewNotification && (
-        <Row>
-          <Col>
-            <Alert variant="success">{reviewNotification}</Alert>
-          </Col>
-        </Row>
-      )}
-
-      <Row>
-        <Col>
-          <ReviewForm handleReviewFormSubmit={handleReviewFormSubmit} />
-        </Col>
-      </Row>
-    </Container>
+      </Container>
+    </>
   );
 };
 
