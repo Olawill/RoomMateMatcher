5f8981a81ba2a08dd8a0be879731ad2e1244d147
Mandy Liu <manling512@gmail.com>
2024-01-07 18:38:18 -0700

    Import the state and functions related to the favorite button into App.jsx and pass them down to the Listings and ListingItemPage components, and pass them into ListingItem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,18 @@
-import './App.css';
-import './NavigationBar.css';
-import './Header.css';
-import { useState } from "react";
+import "./App.css";
+import "./NavigationBar.css";
+import "./Header.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile";
-import MyMessage from './components/myMessages'
+import MyMessage from "./components/myMessages";
 import Listings from "./components/Listings";
 import ListingItemPage from "./components/ListingItemPage";
 import NavigationBar from "./components/NavigationBar";
-import Header from './components/Header';
+import Header from "./components/Header";
+
+import useApplicationData from "./hooks/useApplicationData";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { state, onFavButtonClick } = useApplicationData();
 
   return (
     <Router>
@@ -23,12 +24,23 @@ function App() {
               <NavigationBar />
               <Header />
               <Profile />
-               <MyMessage />
-             <Listings />
+              <MyMessage />
+              <Listings
+                likedListings={state.likedListings}
+                onFavButtonClick={onFavButtonClick}
+              />
             </div>
           }
         />
-        <Route path="/:listing_id" element={<ListingItemPage />} />
+        <Route
+          path="/:listing_id"
+          element={
+            <ListingItemPage
+              likedListings={state.likedListings}
+              onFavButtonClick={onFavButtonClick}
+            />
+          }
+        />
       </Routes>
     </Router>
   );
