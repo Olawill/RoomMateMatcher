b977d032d8ebdc5ef79906dc08a1cbafb8146d22
Olawale <henry.williams658@gmail.com>
2024-01-11 06:15:01 +0000

    Add logic for no listings

--- frontend/src/components/Listing/MyListings.jsx
+++ frontend/src/components/Listing/MyListings.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import PageLayout from "../PageLayout/PageLayout";
-import { Container } from "react-bootstrap";
+import { Button, Container } from "react-bootstrap";
+import { MdOutlineMapsHomeWork } from "react-icons/md";
+import { GoPlus } from "react-icons/go";
+import { IoPinSharp } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 
 const MyListings = () => {
@@ -14,7 +18,37 @@ const MyListings = () => {
           paddingBottom: '2rem'
         }}
       >
-
+        {
+          !myListings && (
+            <Container style={{paddingBlock: '3rem'}}>
+              <h3>Your Listings</h3>
+              <hr/>
+              <Button
+                variant="outline-primary"
+                size="sm"
+                style={{
+                  width: '100%'
+                }}>
+                <GoPlus />
+                Add a New Listing
+              </Button>
+              <hr/>
+              <MdOutlineMapsHomeWork style={{
+                fontSize: '6rem'}}/>
+              <IoPinSharp style={{
+                fontSize: '1rem', 
+                transform: 'rotate(75deg)'
+              }}
+              />
+              <br/>
+              <small>{`Your Listings will live here`}</small>
+              <br/><br/>
+              <Button as={Link} to="/">
+                Browsing our Listings
+              </Button>
+            </Container>
+          )
+        }
       </Container>
       )}
 
