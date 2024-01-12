1a8957224915acbe6f4c74334c462772a12aebc2
Mandy Liu <manling512@gmail.com>
2024-01-11 20:56:23 -0700

    Refactor useApplicationData to use two axios to keep track of likedListings state all over the app

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
 
 const useApplicationData = () => {
   const [likedListings, setLikedListings] = useState([]);
-  const { isAuthenticated,loginWithRedirect, user } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+
+  useEffect(() => {
+    if (isAuthenticated && user) {
+      axios
+        .get(`/api/user/${user.sub}/favourites`)
+        .then((response) => {
+          console.log("Favourite listings:", response.data);
+          const idList = response.data.map(item => item.id);
+          setLikedListings(idList);
+        })
+        .catch((error) => console.error(error));
+    }
+  }, [isAuthenticated, user]);
 
   const onFavButtonClick = (listingId) => {
     if (!isAuthenticated) {
@@ -14,8 +27,7 @@ const useApplicationData = () => {
     }
   
     const isFavourite = likedListings.includes(listingId);
-    axios.post('/api/user/userId/favourites', {
-      userId: user.sub,
+    axios.post(`/api/user/${user.sub}/favourites`, {
       listingId: listingId,
       isFavourite: !isFavourite
     })
