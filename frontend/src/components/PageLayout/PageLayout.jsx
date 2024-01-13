a5dc203a88c1a7d47f7de85ca7b040ccf2725838
Mandy Liu <manling512@gmail.com>
2024-01-12 18:29:09 -0700

    Refactor components to use isLoading provided by auth0 for better user experience when loading the page

--- frontend/src/components/PageLayout/PageLayout.jsx
+++ frontend/src/components/PageLayout/PageLayout.jsx
@@ -1,14 +1,18 @@
-import NavigationBar from '../Navigation/NavigationBar'
-import Header from '../Navigation/Header'
-import { useState } from 'react';
+import NavigationBar from "../Navigation/NavigationBar";
+import Header from "../Navigation/Header";
+import { useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
-const PageLayout = ({ children }) => {
+const PageLayout = ({ children, requireAuthentication = false }) => {
+  const { isLoading } = useAuth0();
 
   // STATE TO MANAGE THEME
-  const [theme, setTheme] = useState(window.sessionStorage.getItem('appTheme') || 'Light');
+  const [theme, setTheme] = useState(
+    window.sessionStorage.getItem("appTheme") || "Light"
+  );
 
   const getThemeAuto = () => {
-    const option = ['Light', 'Dark'];
+    const option = ["Light", "Dark"];
     const randomVal = Math.random();
 
     return randomVal < 0.5 ? option[0] : option[1];
@@ -19,19 +23,18 @@ const PageLayout = ({ children }) => {
     e.preventDefault();
 
     setTheme(e.target.dataset.theme);
-    window.sessionStorage.setItem('appTheme', e.target.dataset.theme);
-  }
+    window.sessionStorage.setItem("appTheme", e.target.dataset.theme);
+  };
 
   return (
     <>
-      <NavigationBar
-        handleTheme={handleTheme}
-        theme={theme}
-      />
-      <Header darkModeEnabled={theme === 'Dark'} />
-      {children({ getThemeAuto, theme })}
+      <NavigationBar handleTheme={handleTheme} theme={theme} />
+      <Header darkModeEnabled={theme === "Dark"} />
+      {requireAuthentication && isLoading
+        ? "Loading the page..."
+        : children({ getThemeAuto, theme })}
     </>
-  )
-}
+  );
+};
 
 export default PageLayout;
