83ae157bc067f272d818570e61bb5338669396a8
Mandy Liu <manling512@gmail.com>
2024-01-12 18:22:38 -0700

    Refactor Auth0Provider to let redirect url work to keep user logged-in

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,12 +7,16 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const domain = 'dev-h1kihg426xuqx2n0.us.auth0.com';
 const clientId = 'P1ZQJGQ05k2euytM30fOyEDV3pzJlhYS';
 
+const {protocol, host} = window.location
+const location = `${protocol}//${host}`
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={window.location.href || window.location.pathname}
+      // redirectUri={window.location.href || window.location.pathname}
+      authorizationParams={{redirect_uri: location}}
     >
       <App />
     </Auth0Provider>
