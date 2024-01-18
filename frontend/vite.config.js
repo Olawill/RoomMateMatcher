002e0dfb6c4af16248883ae53a3b8c3dacf244c0
sophia ojegba <roseysofia@yahoo.com>
2024-01-18 12:21:40 -0500

    ENV variables1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,23 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_REACT_APP_PRODUCTION_API_URL || 'http://localhost:8003',
-        secure: false,
-        configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: import.meta.env.VITE_REACT_APP_PRODUCTION_API_URL || 'http://localhost:8003',
+  //       secure: false,
+  //       configure: (proxy, _options) => {
+  //         proxy.on('error', (err, _req, _res) => {
+  //           console.log('proxy error', err);
+  //         });
+  //         proxy.on('proxyReq', (proxyReq, req, _res) => {
+  //           console.log('Sending Request to the Target:', req.method, req.url);
+  //         });
+  //         proxy.on('proxyRes', (proxyRes, req, _res) => {
+  //           console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+  //         });
+  //       },
+  //     },
+  //   },
+  // },
 })
